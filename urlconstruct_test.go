6cbe57322b255a2d9985b62872c18ca96e083113
Antonio Gutierrez <chibby0ne@gmail.com>
2018-01-28 22:20:10 +0100

    Refactor urlconstruct_tests.go
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- urlconstruct_test.go
+++ urlconstruct_test.go
@@ -5,22 +5,23 @@ import (
     "testing"
 )
 
-func TestWriteOneParameterQueryToReturnNoAdaptTrueStringWithZeroGPUsGiven(t *testing.T) {
-    var buffer bytes.Buffer
-    writeOneParameterQuery(&buffer, "adapt_q_280x=", "0")
-    actualResult := buffer.String()
-    expectedResult := "adapt_q_280x=0&"
-    if expectedResult != actualResult {
-        t.Fatalf("Expected: %s but got: %s\n", expectedResult, actualResult)
-    }
+var writeOneParameterQueryTestInputOutput = []struct {
+    adaptString string
+    numOfGPUs string
+    expectedOutput string
+} {
+    {"adapt_q_280x=", "0",  "adapt_q_280x=0&"},
+    {"adapt_q_280x=", "1", "adapt_q_280x=1&adapt_280x=true&"},
 }
 
-func TestWriteOneParameterQueryToReturnAdaptTrueStringWithNonZeroGPUsGiven(t *testing.T) {
-    var buffer bytes.Buffer
-    writeOneParameterQuery(&buffer, "adapt_q_280x=", "1")
-    actualResult := buffer.String()
-    expectedResult := "adapt_q_280x=1&adapt_280x=true&"
-    if expectedResult != actualResult {
-        t.Fatalf("Expected: %s but got: %s\n", expectedResult, actualResult)
+func TestWriteOneParameterQuery(t *testing.T) {
+    for _, vals := range writeOneParameterQueryTestInputOutput {
+        var buffer bytes.Buffer
+        writeOneParameterQuery(&buffer, vals.adaptString, vals.numOfGPUs)
+        actualResult := buffer.String()
+        expectedResult := vals.expectedOutput
+        if expectedResult != actualResult {
+            t.Fatalf("Expected: %s but got: %s\n", expectedResult, actualResult)
+        }
     }
 }

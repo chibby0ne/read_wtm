40b18bd7c4288bcbf43d4d1fe85a9d77e3fee353
Antonio Gutierrez <chibby0ne@gmail.com>
2018-01-29 00:26:46 +0100

    Format tests with go fmt
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- urlconstruct_test.go
+++ urlconstruct_test.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "bytes"
-    "testing"
+	"bytes"
+	"testing"
 )
 
 var writeOneParameterQueryTestInputOutput = []struct {
-    adaptString string
-    numOfGPUs string
-    expectedOutput string
-} {
-    {"adapt_q_280x=", "0",  "adapt_q_280x=0&"},
-    {"adapt_q_280x=", "1", "adapt_q_280x=1&adapt_280x=true&"},
+	adaptString    string
+	numOfGPUs      string
+	expectedOutput string
+}{
+	{"adapt_q_280x=", "0", "adapt_q_280x=0&"},
+	{"adapt_q_280x=", "1", "adapt_q_280x=1&adapt_280x=true&"},
 }
 
 func TestWriteOneParameterQuery(t *testing.T) {
-    for _, vals := range writeOneParameterQueryTestInputOutput {
-        var buffer bytes.Buffer
-        writeOneParameterQuery(&buffer, vals.adaptString, vals.numOfGPUs)
-        actualResult := buffer.String()
-        expectedResult := vals.expectedOutput
-        if expectedResult != actualResult {
-            t.Fatalf("Expected: %s but got: %s\n", expectedResult, actualResult)
-        }
-    }
+	for _, vals := range writeOneParameterQueryTestInputOutput {
+		var buffer bytes.Buffer
+		writeOneParameterQuery(&buffer, vals.adaptString, vals.numOfGPUs)
+		actualResult := buffer.String()
+		expectedResult := vals.expectedOutput
+		if expectedResult != actualResult {
+			t.Fatalf("Expected: %s but got: %s\n", expectedResult, actualResult)
+		}
+	}
 }
